53d259396c49a2065abd8384f321140f35897428
Eddie Anokye <eddieanokye27@gmail.com>
2025-03-15 19:02:27 -0400

    Added analysis history page

---
+++ src/pages/AnalysisHistory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AnalysisHistory() {
+    return (
+        <div>
+        <h1>AnalysisHistory</h1>
+        </div>
+    );
+}
+
+export default AnalysisHistory;
