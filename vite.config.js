1619009d87c25e6691461f08f4a6d6613dcec064
rycid <57729652+rycid@users.noreply.github.com>
2025-03-15 19:47:08 -0400

    Integrate Tailwind CSS into the project; update Vite config and comment out unused Tailwind plugin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+  
 })
