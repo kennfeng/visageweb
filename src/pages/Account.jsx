f4ccdba08fca5ca4d366859cc67173d00a731a8e
rycid <57729652+rycid@users.noreply.github.com>
2025-03-15 23:01:55 -0400

    Add Account component for user account management

---
+++ src/pages/Account.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Account() {
+    return (
+        <div>
+            Account
+        </div>
+    )
+};
+
+export default Account; 
+ 
