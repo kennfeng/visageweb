8cf6541043f1ce37ba27adc852c8f501b5824ec3
rycid <57729652+rycid@users.noreply.github.com>
2025-03-16 05:56:48 -0400

    Add logging for password hashing process

--- auth/auth.js
+++ auth/auth.js
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 const saltRounds = 10;
 
 export async function hashPassword(password) {
+    console.log('password hashing...');
     return await bcrypt.hash(password, saltRounds);
 }
 
