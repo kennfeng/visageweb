9e522edd787a16b6320643282e0f10a0b94dbd78
rycid <57729652+rycid@users.noreply.github.com>
2025-03-16 00:04:40 -0400

    Add password hashing and comparison functions using bcrypt

---
+++ auth/auth.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+
+// Hash a password
+const saltRounds = 10;
+
+export async function hashPassword(password) {
+    return await bcrypt.hash(password, saltRounds);
+}
+
+// Compare a password with the hash
+export async function checkMatch(password, hash) {
+    return await bcrypt.compare(password, hash);
+}
+
