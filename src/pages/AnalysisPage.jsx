ae9c068d1305231a0ab481dd255e2ac7b0b4f0e6
Eddie Anokye <eddieanokye27@gmail.com>
2025-03-15 23:09:34 -0400

    Changes to analysispage

--- src/pages/AnalysisPage.jsx
+++ src/pages/AnalysisPage.jsx
@@ -3,12 +3,27 @@ import Webcam from "react-webcam";
 import { Button } from "@/components/ui/button";
 
 function AnalysisPage() {
+    
+    function getFacialAnalysis() {
+        fetch('http://localhost:3000/facialanalysis', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+    
     return (
         <div>
             <Webcam className="m-auto" />
-            <Button className="button"> Analyze! </Button>
-            
-
+            <Button className="button" onClick={getFacialAnalysis}> Analyze! </Button>
         </div>
     );
 }
