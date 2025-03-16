58a88afad549ff11c7ed43e72a7927ff46d6c7ea
Eddie Anokye <eddieanokye27@gmail.com>
2025-03-15 20:25:44 -0400

    Added webcam

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/header'
 import Login from './pages/Login'
+import AnalysisPage from './pages/AnalysisPage'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/analysis" element={<AnalysisPage />} />
         
       </Routes>
     </Router>
